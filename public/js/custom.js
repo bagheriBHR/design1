05627f00b70850ce2f5516477649a593781fc352
bahar <bagheri_bhr@yahoo.com>
2020-07-19 19:53:27 +0430

    add search & profile & teacher_info & print capability in online exam page & dropdown menu

--- public/js/custom.js
+++ public/js/custom.js
@@ -15,6 +15,13 @@ $(window).on('scroll', function () {
     }
 });
 
+$('.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+}, function () {
+    $(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp()
+});
+
+
 // slick---------------------------------------------------------------------------------------
 $(document).on('ready', function() {
     $(".center").slick({
